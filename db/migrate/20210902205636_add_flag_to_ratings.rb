b8661d9d091056a6c5e06a57ba4e2e3882864fcb
Partha Tatikonda <tpardhu561@gmail.com>
2021-09-17 03:20:06 -0400

    applying chnages to recent updates

--- db/migrate/20210902205636_add_flag_to_ratings.rb
+++ db/migrate/20210902205636_add_flag_to_ratings.rb
@@ -1,5 +1,5 @@
 class AddFlagToRatings < ActiveRecord::Migration[6.0]
   def change
-    add_column :ratings, :flag, :boolean, default: false
+    add_column :ratings, :flag, :boolean, true
   end
 end
